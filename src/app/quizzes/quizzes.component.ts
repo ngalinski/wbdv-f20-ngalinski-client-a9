1ec1b3c7b12e1d90be0495f61aef69883ccff52a
Nicholas Galinski <galinski.n@husky.neu.edu>
2020-12-05 15:27:13 -0800

    add quizzes

---
+++ src/app/quizzes/quizzes.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {QuizzesServiceClient} from '../../services/quiz.service.client';
+
+@Component({
+  selector: 'app-quizzes',
+  templateUrl: './quizzes.component.html',
+  styleUrls: ['./quizzes.component.css']
+})
+export class QuizzesComponent implements OnInit {
+
+  constructor(private service: QuizzesServiceClient,
+              private route: ActivatedRoute) {
+  }
+
+  courseId = '';
+  quizzes = [];
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.courseId = params.courseId;
+      this.service.findAllQuizzes()
+          .then(quizzes => this.quizzes = quizzes)
+          .then(quizzes =>
+              this.quizzes.forEach(quiz => {
+                this.service.findAttemptsForQuiz(quiz._id)
+                    .then(attempts => quiz.attempts = attempts);
+              }));
+    });
+  }
+}
